db677381d41b299e569b3f1d812d2acc0dfe99b6
csengekulker <99194032+csengekulker@users.noreply.github.com>
2022-12-05 11:12:44 +0100

    Initial commit

---
+++ src/views/MainFrame.java
@@ -0,0 +1,45 @@
+package views;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+public class MainFrame extends JFrame {
+  JPanel mainPanel;
+
+  public MainFrame() {
+
+    setComponents();
+    setMainPanel();
+    setFrame();
+  }
+
+  private void setComponents() {
+
+  }
+
+  private void setMainPanel() {
+    this.mainPanel = new JPanel();
+
+    this.mainPanel.setLayout(
+      new BorderLayout()
+    );
+
+    this.add(this.mainPanel);
+  }
+
+  private void setFrame() {
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setSize(400, 300);
+    this.setTitle("JAVA GUI TEMPLATE");
+    this.setVisible(true);
+
+    // layout for window
+    this.setLayout(
+      new FlowLayout(
+
+      )
+    );
+  }
+}
