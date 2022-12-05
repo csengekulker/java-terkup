a70be2b0c50b997b46d392460a606484f034a3e8
csengekulker <k71608883429@ktch.hu>
2022-12-05 11:38:39 +0100

    clean code

--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -1,14 +1,13 @@
 package controllers;
 
-import models.MainModel;
 import views.MainFrame;
 
 public class MainController {
   MainFrame mainFrame;
-  MainModel mainModel;
   
   public MainController() {
     this.mainFrame = new MainFrame();
-    this.mainModel = new MainModel();
   }
+
+  
 }
